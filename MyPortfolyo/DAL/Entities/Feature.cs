dc04abd6666146b02e7610fbf26cb412d9695a4e
Dell <Dell@DESKTOP-8SRTQL6>
2025-04-29 14:22:48 +0300

    Revert "EF Core Paketleri Yükelndi.."
    This reverts commit ad6b03878cf38d9a011f9dee8b3b6a7a5e8f11a7.

--- MyPortfolyo/DAL/Entities/Feature.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyPortfolyo.DAL.Entities
-{
-    public class Feature
-    {
-        public int FeatureId { get; set; }
-        public string FeatureDesc { get; set; }
-        public string Title { get; set; }
-    }
-}
