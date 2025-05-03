687db886abd716b4f9db59d72972bb715cf5084d
pc <pc@DESKTOP-PJJV610>
2025-05-03 20:45:47 +0300

    ViewComponentsler eklendi.

---
+++ MyPortfolyo/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolyo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v = "https://i.hizliresim.com/aeprmr5.jpg";
+            return View();
+        }
+    }
+}
