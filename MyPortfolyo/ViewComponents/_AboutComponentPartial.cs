67e6d60c18cc1b93329c81dcc50b04a5f527a6fe
pc <pc@DESKTOP-PJJV610>
2025-05-14 01:55:34 +0300

    Componenetler tamamlandı. DataBase ile dinamikler eklenmeye başlandı.

--- MyPortfolyo/ViewComponents/_AboutComponentPartial.cs
+++ MyPortfolyo/ViewComponents/_AboutComponentPartial.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolyo.DAL.Context;
 
 namespace MyPortfolyo.ViewComponents
 {
     public class _AboutComponentPartial:ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
+        ViewBag.Title = portfolioContext.Abouts.Select(x=>x.Title).FirstOrDefault();
+        ViewBag.AboutSubDesc = portfolioContext.Abouts.Select(x=>x.SubDesc).FirstOrDefault();
+        ViewBag.AboutDetail = portfolioContext.Abouts.Select(x=>x.Details).FirstOrDefault();
             return View();
         }
     }
---
+++ MyPortfolyo/ViewComponents/_StatisticComponenetPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolyo.ViewComponents
+{
+    public class _StatisticComponenetPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
