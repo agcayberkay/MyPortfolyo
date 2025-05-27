2089b1158dc189a029bf904c43fa837a6d153594
pc <pc@DESKTOP-PJJV610>
2025-05-27 05:17:36 +0300

    Hakkımda kısımları eklendi. Temel işlemleri yapıldı.

---
+++ MyPortfolyo/Controllers/AboutController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolyo.DAL.Context;
+using MyPortfolyo.DAL.Entities;
+
+namespace MyPortfolyo.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IActionResult AboutList()
+        {
+            var values = portfolioContext.Abouts.ToList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult UpdateAbout(int id)
+        {
+            var about = portfolioContext.Abouts.Find(id);
+            if (about == null)
+            {
+                return NotFound();
+            }
+            return View(about);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAbout(About about)
+        {
+            portfolioContext.Abouts.Update(about);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+    }
+}
---
+++ MyPortfolyo/Controllers/SkillsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolyo.DAL.Context;
+using MyPortfolyo.DAL.Entities;
+
+namespace MyPortfolyo.Controllers
+{
+    public class SkillsController : Controller
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IActionResult SkillsList()
+        {
+            var values = portfolioContext.Skills.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+
+        public IActionResult CreateSkils()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSkils(Skils skils)
+        {
+            portfolioContext.Skills.Add(skils);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("SkillsList");
+        }
+        public IActionResult DeleteSkils(int id)
+        {
+            var skils = portfolioContext.Skills.Find(id);
+            if (skils != null)
+            {
+                portfolioContext.Skills.Remove(skils);
+                portfolioContext.SaveChanges();
+            }
+            return RedirectToAction("SkillsList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSkils(int id)
+        {
+            var skils = portfolioContext.Skills.Find(id);
+            if (skils == null)
+            {
+                return NotFound();
+            }
+            return View(skils);
+        }
+
+        [HttpPost]
+
+        public IActionResult UpdateSkils(Skils skils)
+        {
+            portfolioContext.Skills.Update(skils);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("SkillsList");
+        }
+    }
+}
